0f690187a59a7eac3932c3df21e4150e1b8e325b
Aditya Karekar <adityakarekar0311@gmail.com>
2025-06-14 16:57:09 +0530

    my project so far

---
+++ app/routers/post.py
@@ -0,0 +1,91 @@
+
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from typing import List, Optional
+
+from sqlalchemy import func
+# from sqlalchemy.sql.functions import func
+from .. import models, schemas, oauth2
+from ..database import get_db
+from ..models import Post
+
+
+router = APIRouter(
+    prefix="/posts",
+    tags=['Posts']
+)
+
+
+# @router.get("/", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.PostOut])
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    print("Current user ID:", current_user.id)
+    
+    print("Fetching posts...")
+
+    if search:
+       posts=db.query(Post).filter(Post.title.contains(search)).limit(limit).offset(skip).all()
+       
+    posts = db.query(Post).limit(limit).offset(skip).all()
+    print(posts)
+
+    return [schemas.PostOut.model_validate(post) for post in posts]
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostSchema)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    print(current_user.id)
+
+    new_post = models.Post(owner_id=current_user.id, **post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+
+    return new_post
+
+
+@router.get("/{id}", response_model=schemas.PostOut)
+def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+
+    post=db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id: {id} was not found")
+
+    return post
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post = post_query.first()
+
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id: {id} does not exist")
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Not authorized to perform requested action")
+
+    post_query.delete(synchronize_session=False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.put("/{id}", response_model=schemas.PostSchema)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post = post_query.first()
+
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id: {id} does not exist")
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Not authorized to perform requested action")
+
+    post_query.update(updated_post.model_dump(), synchronize_session=False)
+
+    db.commit()
+
+    return post_query.first()
---
+++ app/schemas.py
@@ -0,0 +1,56 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        from_attributes = True  # Allows conversion from SQLAlchemy models
+
+
+class PostSchema(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserOut
+
+    class Config:
+        from_attributes = True
+
+
+class PostOut(PostSchema):
+    class Config:
+        from_attributes = True  # Ensure Pydantic can handle SQLAlchemy objects
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: int
