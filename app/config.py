a44c089af9552240b4339d848e9d87e5ae6e9c69
Aditya Karekar <adityakarekar0311@gmail.com>
2025-08-16 13:14:49 +0530

    fix: changed dburl so deployment is smooth

--- app/config.py
+++ app/config.py
@@ -2,14 +2,14 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_password: str
-    database_name: str
-    database_username: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    # database_hostname: str
+    # database_port: str
+    # database_password: str
+    # database_name: str
+    # database_username: str
+    # secret_key: str
+    # algorithm: str
+    # access_token_expire_minutes: int
     ext_db_url: str
 
     class Config:
